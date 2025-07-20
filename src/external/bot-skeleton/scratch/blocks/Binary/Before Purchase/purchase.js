95517d05d1052077613bc329a853904966028034
Tonnierich <richtonnie14@gmail.com>
2025-07-20 14:30:55 +0300

    Update purchase.js

--- src/external/bot-skeleton/scratch/blocks/Binary/Before Purchase/purchase.js
+++ src/external/bot-skeleton/scratch/blocks/Binary/Before Purchase/purchase.js
@@ -1,91 +1,119 @@
-import { localize } from '@deriv-com/translations';
-import { getContractTypeOptions } from '../../../shared';
-import { excludeOptionFromContextMenu, modifyContextMenu } from '../../../utils';
+import { localize } from "@deriv-com/translations"
+import { getContractTypeOptions } from "../../../shared"
+import { excludeOptionFromContextMenu, modifyContextMenu } from "../../../utils"
 
 window.Blockly.Blocks.purchase = {
-    init() {
-        this.jsonInit(this.definition());
+  init() {
+    this.jsonInit(this.definition())
+    // Ensure one of this type per statement-stack
+    this.setNextStatement(false)
+  },
+  definition() {
+    return {
+      message0: localize("Purchase {{ contract_type }}", { contract_type: "%1" }),
+      args0: [
+        {
+          type: "field_dropdown",
+          name: "PURCHASE_LIST",
+          options: [["", ""]],
+        },
+      ],
+      // New message and args for "Allow Bulk Purchase"
+      message1: localize("Allow Bulk Purchase: %1"),
+      args1: [
+        {
+          type: "field_dropdown",
+          name: "ALLOW_BULK_PURCHASE",
+          options: [
+            [localize("No"), "FALSE"],
+            [localize("Yes"), "TRUE"],
+          ], // Default to No
+          value: "FALSE",
+        },
+      ],
+      // New message and args for "No. of Trades"
+      message2: localize("No. of Trades: %1"),
+      args2: [
+        {
+          type: "field_number",
+          name: "NO_OF_TRADES",
+          value: 1,
+          min: 1,
+          precision: 0, // Ensure it's an integer
+        },
+      ],
+      previousStatement: null,
+      colour: window.Blockly.Colours.Special1.colour,
+      colourSecondary: window.Blockly.Colours.Special1.colourSecondary,
+      colourTertiary: window.Blockly.Colours.Special1.colourTertiary,
+      tooltip: localize("This block purchases contract of a specified type."),
+      category: window.Blockly.Categories.Before_Purchase,
+    }
+  },
+  meta() {
+    return {
+      display_name: localize("Purchase"),
+      description: localize(
+        "Use this block to purchase the specific contract you want. You may add multiple Purchase blocks together with conditional blocks to define your purchase conditions. This block can only be used within the Purchase conditions block.",
+      ),
+      key_words: localize("buy"),
+    }
+  },
+  onchange(event) {
+    if (!this.workspace || window.Blockly.derivWorkspace.isFlyoutVisible || this.workspace.isDragging()) {
+      return
+    }
+    if (event.type === window.Blockly.Events.BLOCK_CREATE && event.ids.includes(this.id)) {
+      this.populatePurchaseList(event)
+    } else if (event.type === window.Blockly.Events.BLOCK_CHANGE) {
+      if (event.name === "TYPE_LIST" || event.name === "TRADETYPE_LIST") {
+        this.populatePurchaseList(event)
+      }
+    } else if (event.type === window.Blockly.Events.BLOCK_DRAG && !event.isStart && event.blockId === this.id) {
+      const purchase_type_list = this.getField("PURCHASE_LIST")
+      const purchase_options = purchase_type_list.menuGenerator_ // eslint-disable-line
+      if (purchase_options[0][0] === "") {
+        this.populatePurchaseList(event)
+      }
+    }
+  },
+  populatePurchaseList(event) {
+    const trade_definition_block = this.workspace.getTradeDefinitionBlock()
+    if (trade_definition_block) {
+      const trade_type_block = trade_definition_block.getChildByType("trade_definition_tradetype")
+      const trade_type = trade_type_block.getFieldValue("TRADETYPE_LIST")
+      const contract_type_block = trade_definition_block.getChildByType("trade_definition_contracttype")
+      const contract_type = contract_type_block.getFieldValue("TYPE_LIST")
+      const purchase_type_list = this.getField("PURCHASE_LIST")
+      const purchase_type = purchase_type_list.getValue()
+      const contract_type_options = getContractTypeOptions(contract_type, trade_type)
+      purchase_type_list.updateOptions(contract_type_options, {
+        default_value: purchase_type,
+        event_group: event.group,
+        should_pretend_empty: true,
+      })
+    }
+  },
+  customContextMenu(menu) {
+    const menu_items = [localize("Enable Block"), localize("Disable Block")]
+    excludeOptionFromContextMenu(menu, menu_items)
+    modifyContextMenu(menu)
+  },
+  restricted_parents: ["before_purchase"],
+}
 
-        // Ensure one of this type per statement-stack
-        this.setNextStatement(false);
-    },
-    definition() {
-        return {
-            message0: localize('Purchase {{ contract_type }}', { contract_type: '%1' }),
-            args0: [
-                {
-                    type: 'field_dropdown',
-                    name: 'PURCHASE_LIST',
-                    options: [['', '']],
-                },
-            ],
-            previousStatement: null,
-            colour: window.Blockly.Colours.Special1.colour,
-            colourSecondary: window.Blockly.Colours.Special1.colourSecondary,
-            colourTertiary: window.Blockly.Colours.Special1.colourTertiary,
-            tooltip: localize('This block purchases contract of a specified type.'),
-            category: window.Blockly.Categories.Before_Purchase,
-        };
-    },
-    meta() {
-        return {
-            display_name: localize('Purchase'),
-            description: localize(
-                'Use this block to purchase the specific contract you want. You may add multiple Purchase blocks together with conditional blocks to define your purchase conditions. This block can only be used within the Purchase conditions block.'
-            ),
-            key_words: localize('buy'),
-        };
-    },
-    onchange(event) {
-        if (!this.workspace || window.Blockly.derivWorkspace.isFlyoutVisible || this.workspace.isDragging()) {
-            return;
-        }
+window.Blockly.JavaScript.javascriptGenerator.forBlock.purchase = (block) => {
+  const purchaseList = block.getFieldValue("PURCHASE_LIST")
+  const allowBulkPurchase = block.getFieldValue("ALLOW_BULK_PURCHASE") // 'TRUE' or 'FALSE'
+  const noOfTrades = block.getFieldValue("NO_OF_TRADES") // number
 
-        if (event.type === window.Blockly.Events.BLOCK_CREATE && event.ids.includes(this.id)) {
-            this.populatePurchaseList(event);
-        } else if (event.type === window.Blockly.Events.BLOCK_CHANGE) {
-            if (event.name === 'TYPE_LIST' || event.name === 'TRADETYPE_LIST') {
-                this.populatePurchaseList(event);
-            }
-        } else if (event.type === window.Blockly.Events.BLOCK_DRAG && !event.isStart && event.blockId === this.id) {
-            const purchase_type_list = this.getField('PURCHASE_LIST');
-            const purchase_options = purchase_type_list.menuGenerator_; // eslint-disable-line
+  // Convert 'TRUE'/'FALSE' strings to boolean literals for JavaScript
+  const allowBulkJs = allowBulkPurchase === "TRUE" ? "true" : "false"
 
-            if (purchase_options[0][0] === '') {
-                this.populatePurchaseList(event);
-            }
-        }
-    },
-    populatePurchaseList(event) {
-        const trade_definition_block = this.workspace.getTradeDefinitionBlock();
-
-        if (trade_definition_block) {
-            const trade_type_block = trade_definition_block.getChildByType('trade_definition_tradetype');
-            const trade_type = trade_type_block.getFieldValue('TRADETYPE_LIST');
-            const contract_type_block = trade_definition_block.getChildByType('trade_definition_contracttype');
-            const contract_type = contract_type_block.getFieldValue('TYPE_LIST');
-            const purchase_type_list = this.getField('PURCHASE_LIST');
-            const purchase_type = purchase_type_list.getValue();
-            const contract_type_options = getContractTypeOptions(contract_type, trade_type);
-
-            purchase_type_list.updateOptions(contract_type_options, {
-                default_value: purchase_type,
-                event_group: event.group,
-                should_pretend_empty: true,
-            });
-        }
-    },
-    customContextMenu(menu) {
-        const menu_items = [localize('Enable Block'), localize('Disable Block')];
-        excludeOptionFromContextMenu(menu, menu_items);
-        modifyContextMenu(menu);
-    },
-    restricted_parents: ['before_purchase'],
-};
-
-window.Blockly.JavaScript.javascriptGenerator.forBlock.purchase = block => {
-    const purchaseList = block.getFieldValue('PURCHASE_LIST');
-
-    const code = `Bot.purchase('${purchaseList}');\n`;
-    return code;
-};
+  // This generates the JavaScript code for the block.
+  // You might need to adjust how 'allowBulkJs' and 'noOfTrades' are used
+  // based on how your Deriv API wrapper (Bot.purchase) expects these parameters
+  // for bulk trading. This example assumes an options object.
+  const code = `Bot.purchase('${purchaseList}', { allowBulk: ${allowBulkJs}, numTrades: ${noOfTrades} });\n`
+  return code
+}
